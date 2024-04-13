f0573afc7b23c8c7f3e471430ffadabf0f5e3e40
KupchinskyDev <kupchinsky.dv@zerolab.tech>
2024-04-13 22:36:54 +0300

    add zip search

---
+++ sync_utils.py
@@ -0,0 +1,58 @@
+import os
+import docx2txt  # Для чтения .doc файлов
+import textract
+import re
+import docx
+import asyncio
+import translators as ts
+from pypdf import PdfReader
+from striprtf.striprtf import rtf_to_text
+
+
+def clean_text(text):
+    # Удаление всех символов, кроме букв и цифр
+    cleaned_text = re.sub(r'[^a-zA-Z0-9а-яА-Я\s]', '', text)
+    # Удаление лишних пробелов
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+    cleaned_text = cleaned_text.lower()
+    return cleaned_text[:8000]
+
+# Функция для чтения текста из файлов .docx
+def read_docx(file_path):
+    doc = docx.Document(file_path)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text
+    text = clean_text(text)
+    return text
+
+# Функция для чтения текста из файлов .doc
+def read_doc(file_path):
+    text = textract.process(file_path)
+    text = text.decode("utf-8")
+    text = clean_text(text)
+    return text
+
+def read_pdf(file_path):
+    reader = PdfReader(file_path)
+    number_of_pages = len(reader.pages)
+    page = reader.pages[0]
+    text = page.extract_text()
+    text = clean_text(text)
+    return text
+
+
+def read_rtf(file_path):
+    with open(file_path, 'r') as file:
+        text = file.read()
+    text = rtf_to_text(text)
+    text = clean_text(text)
+    return text
+
+def translator_func(text, to_language="ru"):
+    translator = "bing"
+    ts_text = ts.translate_text(
+                  text,
+                  translator=translator,
+                  to_language=to_language)
+    return ts_text
